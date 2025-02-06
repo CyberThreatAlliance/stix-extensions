a6add8068ea0aaf4dc79d5c519ac142dd17d1e16
Jason Kendall <jaskenda@cisco.com>
2025-02-06 08:50:25 -0500

    Move analysis

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -3,7 +3,7 @@
 import pytest
 import stix2
 import json
-from cta.analysis import ENGINE_ARTIFACTS_EXTENSION_ID, AnalysisEngineArtifacts
+from cta.stix.v21.extensions.analysis import ENGINE_ARTIFACTS_EXTENSION_ID, AnalysisEngineArtifacts
 
 
 @pytest.fixture
